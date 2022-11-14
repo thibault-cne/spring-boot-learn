fa558c3b007fbbd3e7f06fcdafdf198408d29a4c
thibault-cne <thibault.chene23@gmail.com>
2022-11-14 15:38:07 +0100

    [+] Hello world

--- src/main/java/com/example/springbootlearn/DemoApplication.java
+++ src/main/java/com/example/springbootlearn/DemoApplication.java
@@ -2,9 +2,19 @@ package com.example.springbootlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class DemoApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
